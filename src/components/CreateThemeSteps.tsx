b2b8d5c596f421f7f9164ddc9f2dcb5440edc3e9
Cxndr <livewellandmakethings@gmail.com>
2024-11-13 06:00:11 +0000

    setup types for imageSet

---
+++ src/components/CreateThemeSteps.tsx
@@ -0,0 +1,18 @@
+
+
+export default function CreateThemeSteps() {
+
+
+  return (
+    <>
+      <ul className="steps w-full">
+        <li className="step step-accent my-auto">Playmats</li>
+        <li className="step my-auto">Menus</li>
+        <li className="step my-auto">Card Backs</li>
+        <li className="step my-auto">Don Cards</li>
+        <li className="step my-auto">Cards</li>
+      </ul>
+      <button className="btn btn-success my-auto text-xl shadow-sm shadow-black">Download Set</button>
+    </>
+  )
+}
---
+++ src/types/imageSet.ts
@@ -0,0 +1,50 @@
+import { Jimp } from "jimp";
+
+export type EdgeStyle = "square" | "rounded-small" | "rounded-med" | "rounded-large";
+export type OverlayStyle = "none" | "area-markers" | "area-markers-text";
+
+export type ThemeImage = {
+  selection: string;
+  image: typeof Jimp | null;
+}
+
+export type ImageSet = {
+  playmats: {
+    overlay: OverlayStyle,
+    edgeStyle: EdgeStyle,
+    shadow: boolean,
+    images: {
+      Black: ThemeImage, BlackYellow: ThemeImage, Blue: ThemeImage, BlueBlack: ThemeImage, BluePurple: ThemeImage, BlueYellow: ThemeImage, Green: ThemeImage, GreenBlack: ThemeImage, GreenBlue: ThemeImage, GreenPurple: ThemeImage, GreenYellow: ThemeImage, Purple: ThemeImage, PurpleBlack: ThemeImage, PurpleYellow: ThemeImage, Red: ThemeImage, RedBlack: ThemeImage, RedBlue: ThemeImage, RedGreen: ThemeImage, RedPurple: ThemeImage, RedYellow: ThemeImage, Yellow: ThemeImage;
+    };
+  };
+  menus: {
+    bgImages: {
+      background: ThemeImage,
+      deckeditbackground: ThemeImage,
+    };
+  };
+  cardBacks: {
+    overlay: OverlayStyle,
+    edgeStyle: EdgeStyle,
+    shadow: boolean,
+    images: {
+      CardBackRegular: ThemeImage,
+      CardBackDon: ThemeImage,
+    };
+  };
+  donCards: {
+    overlay: OverlayStyle,
+    edgeStyle: EdgeStyle,
+    shadow: boolean,
+    images: {
+      DonCard: ThemeImage,
+    };
+  };
+  cards: {
+    edgeStyle: EdgeStyle,
+    shadow: boolean,
+    images: {
+      [key: string]: ThemeImage,
+    };
+  };
+}
