dff286962eaff6f3cbfe9a8b59af7b9f0056bd0f
Cxndr <livewellandmakethings@gmail.com>
2024-12-15 07:51:32 +0000

    added playmat sizing, shadows, edge style and overlays

--- src/components/SelectShadowStyle.tsx
+++ src/components/SelectShadowStyle.tsx
@@ -1,13 +1,33 @@
 
+import { ImageSet } from "@/utils/imageSet"
 
+type SelectShadowStyleProps = {
+  settings: ImageSet["playmats"], // todo: make this modular to work with other than playmats.
+  updatePlaymatPreview: () => void;
+}
 
-export default function SelectShadowStyle() {
+export default function SelectShadowStyle({settings, updatePlaymatPreview}:SelectShadowStyleProps) {
 
+  function returnSelection(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.currentTarget.checked) {
+      settings.shadow = true;
+      updatePlaymatPreview();
+    }
+    else {
+      settings.shadow = false;
+      updatePlaymatPreview();
+    }
+  }
 
   return (
     <label className="label">
       <span className="label-text text-zinc-50 pr-2 text-xl">Shadow: </span>
-      <input type="checkbox" name="shadow-style" className="toggle toggle-accent toggle-lg"/>
+      <input 
+        type="checkbox" 
+        name="shadow-style" 
+        className="toggle toggle-accent toggle-lg"
+        onChange={returnSelection}
+      />
     </label>
   )
 }
