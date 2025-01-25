33c3e22b45f6bb0b6b498b1d9f1acb5d7dab1993
Cxndr <livewellandmakethings@gmail.com>
2025-01-25 04:24:16 +0000

    switched swcMinify to false - fixes build bug with jimp, see comment in next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   images: {
     domains: ['res.cloudinary.com']
-  }
+  },
+  swcMinify: false, // switches to tensor minification - bug regarding jimp and nextjs: https://github.com/jimp-dev/jimp/issues/1344
 };
 
 
