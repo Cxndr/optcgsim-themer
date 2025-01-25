5a0e832616a1c41ead63ddb43b6d2d4004606600
Cxndr <livewellandmakethings@gmail.com>
2025-01-25 04:15:03 +0000

    moved artwork images to cloudinary implementation

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,40 +1,64 @@
-import path from "path";
-import fs from "fs";
-// import CreateTheme from "@/components/CreateTheme";
+import CreateTheme from "@/components/CreateTheme";
 import { ThemeImage } from "@/utils/imageSet";
+
+import { v2 as cloudinary } from "cloudinary";
+
+interface CloudinaryResource {
+  public_id: string;
+  secure_url: string;
+  asset_folder: string;
+  context?: {
+    custom?: Record<string, string>; // Optional metadata
+  };
+  tags?: string[];
+}
+
+interface CloudinarySearchResponse {
+  resources: CloudinaryResource[];
+}
 
 
 export default async function CreatePage() {
 
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+    api_key: process.env.CLOUDINARY_API_KEY!,
+    api_secret: process.env.CLOUDINARY_API_SECRET!,
+  });
+
   async function getArtImages() {
-    "use client";
+    const folderPath = "optcgsim-themer";
     const images: ThemeImage[] = [];
-    const imgRelPath = "/img/art";
-    const imgFolder = path.resolve(process.cwd(), "public", "img", "art"); // Absolute path to img/art inside public
-    const folders = fs.readdirSync(imgFolder);
-    for (const folder of folders) {
-      const subFolderPath = path.join(imgFolder, folder);
-      const stats = fs.statSync(subFolderPath);
-      if (stats.isDirectory()) {
-        const files = fs.readdirSync(subFolderPath);
-        files.forEach((file, index) => {
-          images.push({
-            name: `${folder}-${index}`, //file.split(".")[0],
-            src: `${imgRelPath}/${folder}/${file}`,
-          });
+
+    try {
+      const response: CloudinarySearchResponse = await cloudinary.search
+      .expression(`folder:${folderPath}/*`) // Search API handles dynamic folders better
+      .max_results(500)
+      .sort_by('public_id', 'asc')
+      .execute();
+  
+      response.resources.forEach((file, index) => {
+        images.push({
+          name: `${file.asset_folder.split("/").pop()}-${index}`,
+          src: file.secure_url,
         });
-      }
+      });
+
+      images.sort((a,b) => (a.name || '').localeCompare(b.name || ''));
+  
+      return images;
+    } catch (error) {
+      console.error("Error fetching images from Cloudinary:", error);
+      return [];
     }
-    return images as ThemeImage[];
   }
 
   const artImages = await getArtImages();
-  console.log(artImages);
 
   return (
     <div className="w-full h-full p-10 gap-8 flex flex-col justify-center items-center">
 
-      {/* <CreateTheme artImages={artImages}/> */}
+      <CreateTheme artImages={artImages}/>
 
     </div>
   )
